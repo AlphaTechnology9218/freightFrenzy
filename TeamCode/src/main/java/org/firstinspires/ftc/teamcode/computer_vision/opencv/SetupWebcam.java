89bc24910b0de408e3b7b14eb4a5b2e8b30f202c
Alpha Technology <alphatechjac@gmail.com>
2022-03-21 05:54:21 -0300

    Completed Webcam Setup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/computer_vision/opencv/SetupWebcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/computer_vision/opencv/SetupWebcam.java
@@ -1,29 +1,69 @@
 package org.firstinspires.ftc.teamcode.computer_vision.opencv;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 
-@Autonomous(name = "OpenCV Setup Webcam", group = "Computer Vision")
+@TeleOp(name = "OpenCV Setup Webcam", group = "Computer Vision")
 public class SetupWebcam extends LinearOpMode {
+    int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+            "cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+
+    public WebcamName robotWebcam = hardwareMap.get(WebcamName.class, "Robot Webcam");
+    public OpenCvCamera camera = OpenCvCameraFactory.getInstance().createWebcam(robotWebcam,
+            cameraMonitorViewId);
+    // ContourPipeline myPipeline;
+    /*********************************************************************************************
+     * cameraMonitorViewId -                                                                     *
+     * robotWebcam -                                                                             *
+     * camera -                                                                                  *
+     *********************************************************************************************/
+
+    // TODO: Create the first Pipeline
+
     @Override
     public void runOpMode() {
-        initializeWebcam();
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened() {
+                camera.setViewportRenderer(OpenCvCamera.ViewportRenderer.GPU_ACCELERATED);
+                /*
+                 * Commonly Supported Resolutions:
+                 * 320x240
+                 * 640x480
+                 * 1280x720
+                 * 1920x1080
+                 * */
+                camera.startStreaming(1280, 720, OpenCvCameraRotation.UPRIGHT);
+                // camera.setPipeline(yourPipeline);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+                telemetry.addData("Status:", "An error occurred with OpenCV!");
+            }
+        });
+
+        if (opModeIsActive()) {
+            telemetry.addData("Status: ", "Op Mode is Activated");
+            while (opModeIsActive()) {
+                stopWebcamView();
+            }
+            telemetry.update();
+        }
     }
 
-    public void initializeWebcam() {
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
-                "cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-
-        WebcamName robotWebcam = hardwareMap.get(WebcamName.class, "Robot Webcam");
-
-        // With live preview
-        OpenCvCamera camera = OpenCvCameraFactory.getInstance().createWebcam(robotWebcam,
-                cameraMonitorViewId);
-
-        // Without a live preview
-        // OpenCvCamera camera = OpenCvCameraFactory.getInstance().createWebcam(robotWebcam);
+    public void stopWebcamView() {
+        if (gamepad2.y) {
+            camera.pauseViewport();
+        } else if (gamepad2.a) {
+            camera.resumeViewport();
+        }
     }
 }

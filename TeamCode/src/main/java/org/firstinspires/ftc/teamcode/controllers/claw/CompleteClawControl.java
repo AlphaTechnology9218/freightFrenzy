17d9e871ab1a7a864d9770115cbee76e89ac91ee
JAC0295871 <pedro.otavi4@gmail.com.br>
2022-04-12 12:12:17 -0300

    Claw Updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/claw/CompleteClawControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/claw/CompleteClawControl.java
@@ -1,4 +1,17 @@
 package org.firstinspires.ftc.teamcode.controllers.claw;
 
-public class CompleteClawControl {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class CompleteClawControl extends OpMode {
+    ClawControlComponents servo = new ClawControlComponents();
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
 }

a388a0cbcf2d7298792577f14df437d28f141c4b
Eduardo <eduardomelodefarias@gmail.com>
2022-04-19 15:17:54 -0300

    Autonomous Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/initial_tests/InitialAutoTests.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/initial_tests/InitialAutoTests.java
@@ -4,11 +4,13 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.computer_vision.tensorflow.ObjectsDetected;
+import org.firstinspires.ftc.teamcode.controllers.carousel.CarouselControlMotor;
 import org.firstinspires.ftc.teamcode.locomotion.hardware.MotorEncodersSetup;
 
 @Autonomous(name = "Test Autonomous", group = "Autonomous")
 public class InitialAutoTests extends LinearOpMode {
     MotorEncodersSetup robot = new MotorEncodersSetup(); // access robot components configuration
+    CarouselControlMotor carousel = new CarouselControlMotor();
 
     @Override
     public void runOpMode() {
@@ -38,11 +40,14 @@ public class InitialAutoTests extends LinearOpMode {
         sleep(5000);
         switch (ObjectsDetected.DuckBarcode){
             case 1:
+                //TODO:Program the claw to place the duck in the first level
                 break;
             case 2:
+                //TODO:Program the claw to place the duck in the second level
                 assert true;
                 break;
             case 3:
+                //TODO:Program the claw to place the duck in the third level
                 assert false;
                 break;
             default:
@@ -59,10 +64,10 @@ public class InitialAutoTests extends LinearOpMode {
     public void goingToCarousel(){
         robot.moveRobot(500, 500,500,500, 1);
         sleep(5000);
+        carousel.rightDuck.setPower(1);
         walkingSideways();
     }
 
-    // TODO: Knock the duck with carousel controller
 
     public void walkingSideways(){
         robot.moveRobot(-100, 100,100,-100, 1);

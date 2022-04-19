4b5b7fa73040128dc62116a173a451926dabec4a
JAC0295871 <pedro.otavi4@gmail.com.br>
2022-04-19 12:06:28 -0300

    New Distance Sensor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/abilities/SensorTouch.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.abilities;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+
+public class SensorTouch extends LinearOpMode {
+    public boolean isPressed;
+    DigitalChannel digitalTouch;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        digitalTouch = hardwareMap.get(DigitalChannel.class, "Digital Sensor");
+        digitalTouch.setMode(DigitalChannel.Mode.INPUT);
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+            if (digitalTouch.getState()) {
+                telemetry.addData("Digital Touch: ", "Is not pressed");
+                isPressed = false;
+            } else {
+                telemetry.addData("Digital Touch: ", "Is pressed");
+                isPressed = true;
+            }
+            telemetry.update();
+        }
+    }
+}

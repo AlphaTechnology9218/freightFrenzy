b791778e710800db1b9eecadd892556eb107fa19
jac0295871 <alphatechjac@gmail.com>
2022-05-12 11:50:19 -0300

    IMU Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/initial_tests/autonomous/TestRoundIMU.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/initial_tests/autonomous/TestRoundIMU.java
@@ -3,24 +3,25 @@ package org.firstinspires.ftc.teamcode.initial_tests.autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.abilities.SensorIMU;
+import org.firstinspires.ftc.teamcode.locomotion.autonomous.MotorEncodersSetup;
 import org.firstinspires.ftc.teamcode.robot_components.MotorComponents;
 
 @Autonomous(name = "Test IMU", group = "Autonomous Test")
 public class TestRoundIMU extends LinearOpMode {
     SensorIMU sense = new SensorIMU();
-    MotorComponents motors = new MotorComponents();
+    MotorEncodersSetup motors = new MotorEncodersSetup();
 
     @Override
     public void runOpMode() throws InterruptedException {
-        motors.init(hardwareMap);
+        motors.runOpMode(hardwareMap);
         sense.init(hardwareMap);
         waitForStart();
         while(opModeIsActive()){
             sense.runOpMode();
             if(sense.degree > 5){
-                motors.motorPower(-0.07f, -0.1f, 0.06f, 0.1f);
+                motors.moveRobot(5,5,5,5,0.5);
             } else if (sense.degree < -5){
-                motors.motorPower(0.07f, 0.1f, -0.06f, -0.1f);
+                motors.moveRobot(5,5,5,5,0.5);
             }
         }
     }

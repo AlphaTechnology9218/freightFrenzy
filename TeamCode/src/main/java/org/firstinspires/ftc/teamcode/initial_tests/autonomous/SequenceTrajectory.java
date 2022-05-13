356d400449a4a37dec620c8cf973b69cdee58178
Eduardo <eduardomelodefarias@gmail.com>
2022-05-13 16:37:14 -0300

    Odometry Update

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/initial_tests/autonomous/SequenceTrajectory.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.initial_tests.autonomous;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.odometry.control.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.odometry.traject.TrajectorySequence;
+
+public class SequenceTrajectory extends LinearOpMode {
+    public static double DISTANCE = 48; // in
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        Pose2d startPose = new Pose2d(-DISTANCE / 2, -DISTANCE / 2, 0);
+
+        drive.setPoseEstimate(startPose);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (!isStopRequested()) {
+            TrajectorySequence trajSeq = drive.trajectorySequenceBuilder(startPose)
+                    .forward(DISTANCE)
+                    .turn(Math.toRadians(90))
+                    .forward(DISTANCE)
+                    .turn(Math.toRadians(90))
+                    .forward(DISTANCE)
+                    .turn(Math.toRadians(90))
+                    .forward(DISTANCE)
+                    .turn(Math.toRadians(90))
+                    .build();
+            drive.followTrajectorySequence(trajSeq);
+        }
+    }
+}

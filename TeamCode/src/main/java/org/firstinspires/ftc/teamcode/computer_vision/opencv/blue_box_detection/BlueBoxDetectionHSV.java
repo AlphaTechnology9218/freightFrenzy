2223a833d1afb429f1ab3da47474ef5be9c0245d
JAC0295871 <eduardomelodefarias@gmail.com>
2022-05-17 19:16:20 -0300

    OpenCV Update

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/computer_vision/opencv/blue_box_detection/BlueBoxDetectionHSV.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.computer_vision.opencv.blue_box_detection;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.opencv.core.Mat;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class BlueBoxDetectionHSV extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    class BlueBoxVision extends OpenCvPipeline {
+
+        @Override
+        public Mat processFrame(Mat input) {
+            return null;
+        }
+    }
+}

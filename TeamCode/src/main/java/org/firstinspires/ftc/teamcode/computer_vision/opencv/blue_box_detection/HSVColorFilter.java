88b02eb02000737c1a0460d40361cfa097628e9d
jac0295871 <alphatechjac@gmail.com>
2022-05-18 08:27:44 -0300

    OpenCV Blue Box Updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/computer_vision/opencv/blue_box_detection/HSVColorFilter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/computer_vision/opencv/blue_box_detection/HSVColorFilter.java
@@ -2,7 +2,11 @@ package org.firstinspires.ftc.teamcode.computer_vision.opencv.blue_box_detection
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 public class HSVColorFilter extends OpMode {
@@ -18,9 +22,19 @@ public class HSVColorFilter extends OpMode {
     }
 
     class BlueBoxVision extends OpenCvPipeline {
+        Mat mat  = new Mat();
+        Mat mask = new Mat();
 
         @Override
         public Mat processFrame(Mat input) {
+            Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV);
+
+            // lower and upper bound for BLUE
+            Scalar lowerBound = new Scalar(221, 100, 100);
+            Scalar upperBound = new Scalar(240, 255, 255);
+
+            Core.inRange(mat, lowerBound, upperBound, mask);
+
             return null;
         }
     }

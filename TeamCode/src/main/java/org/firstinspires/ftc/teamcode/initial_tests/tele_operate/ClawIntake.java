733a9ae5fbbedea904b894c8db800f379aee5e20
Eduardo <eduardomelodefarias@gmail.com>
2022-05-19 09:24:02 -0300

    Odometry Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/initial_tests/tele_operate/ClawIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/initial_tests/tele_operate/ClawIntake.java
@@ -4,33 +4,35 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name = "Intake Test", group = "claw tests")
 public class ClawIntake extends LinearOpMode {
-    private DcMotor intake, claw;
-    private double position;
+    private DcMotor intake, clawL, clawR;
 
     @Override
     public void runOpMode() throws InterruptedException {
         intake = hardwareMap.get(DcMotor.class, "Intake");
-        claw = hardwareMap.get(DcMotor.class, "claw");
+        clawL = hardwareMap.get(DcMotor.class, "clawR");
+        clawR = hardwareMap.get(DcMotor.class, "clawL");
 
         intake.setDirection(DcMotor.Direction.FORWARD);
-        claw.setDirection(DcMotor.Direction.REVERSE);
+        clawL.setDirection(DcMotor.Direction.REVERSE);
+        clawR.setDirection(DcMotor.Direction.FORWARD);
 
-        claw.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        claw.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        clawL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        clawR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        while(!isStarted()) {
-            position = claw.getCurrentPosition();
-            telemetry.addData("position", position);
-            telemetry.update();
-        }
+        clawL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        clawR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        boolean dir = true;
+       waitForStart();
+
         boolean full = false;
+        boolean dir = true;
 
         while(opModeIsActive()) {
+            // INTAKE
             if(gamepad1.y && !full) {
                 intake.setPower(1);
                 if(gamepad1.y) {
@@ -46,24 +48,40 @@ public class ClawIntake extends LinearOpMode {
                 }
             }
 
-            if(gamepad1.x) {
-                dir = true;
-                claw.setPower(1);
-                claw.setTargetPosition(200);
-                claw.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            }
-            else if(gamepad1.b) {
+            // GO
+            if(gamepad1.x && dir) {
+                clawL.setTargetPosition(1);
+                clawR.setTargetPosition(1);
+
+                clawL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                clawR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                clawL.setPower(0.7);
+                clawR.setPower(0.7);
+
+                sleep(500);
                 dir = false;
-                claw.setPower(-0.1);
-                claw.setTargetPosition(0);
-                claw.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             }
-            if (!dir && ( claw.getTargetPosition() < 5 || claw.getTargetPosition() < 5)) {
-                claw.setPower(0);
-                claw.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+            // BACK
+            if(gamepad1.y && !dir) {
+                clawL.setTargetPosition(0);
+                clawR.setTargetPosition(0);
+
+                clawL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                clawR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                clawR.setPower(-0.1);
+                clawL.setPower(-0.1);
+
+                dir = true;
             }
-            telemetry.addData("position", position);
-            telemetry.update();
+            if (!dir && ( clawL.getTargetPosition() < 5 || clawR.getTargetPosition() < 5)) {
+                clawL.setPower(0);
+                clawR.setPower(0);
+                clawL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                clawR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            }
         }
     }
     //https://www.ctrlaltftc.com/practical-examples/ftc-motor-control

1c9bc228b3e56a0fe344565129d65bb95c651c23
jac0295828 <eduardomelodefarias@gmail.com>
2022-05-20 14:57:18 -0300

    PID Tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odometry/control/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/odometry/control/DriveConstants.java
@@ -19,8 +19,8 @@ public class DriveConstants {
         public static double kA = 0;
         public static double kStatic = 0; // don't change!
 
-        public static double MAX_VEL = 18.651848; // 80% of max velocity
-        public static double MAX_ACCEL = 18.651848;
+        public static double MAX_VEL = 20.1798392; // 80% of max velocity
+        public static double MAX_ACCEL = 20.1798392;
         public static double MAX_ANG_VEL = Math.toRadians(182.073254);
         public static double MAX_ANG_ACCEL = Math.toRadians(182.073254);
 
@@ -36,7 +36,7 @@ public class DriveConstants {
                 return 32767 / ticksPerSecond;
         }
 }
-         // max velocity = 23,31481
+         // max velocity = 25.224799
         // documentation: https://docs.google.com/document/d/1tyWrXDfMidwYyP_5H4mZyVgaEswhOC35gvdmP-V-5hA/edit#heading=h.61g9ixenznbx
 
 

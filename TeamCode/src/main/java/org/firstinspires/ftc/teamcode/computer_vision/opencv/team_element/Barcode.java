0934e78bd91e4532fbddcefaaf809a315f1c331c
Alpha Technology <alphatechjac@gmail.com>
2022-05-22 21:21:01 -0300

    Team Element Synchronization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/computer_vision/opencv/team_element/Barcode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/computer_vision/opencv/team_element/Barcode.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.computer_vision.opencv.team_element;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.openftc.easyopencv.OpenCvCamera;
@@ -9,18 +10,20 @@ import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvInternalCamera;
 
 @Autonomous(name = "Barcode", group = "Computer Vision")
-public class Barcode extends OpMode {
+public class Barcode extends LinearOpMode {
     OpenCvCamera camera;
-    YCbCrChannelTest pipeline;
+    YCbCrChannelTest vision = new YCbCrChannelTest();
+
+    public int robot = vision.position;
 
     @Override
-    public void init() {
+    public void runOpMode() {
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier
                 ("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         camera = OpenCvCameraFactory.getInstance().createInternalCamera
                 (OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
 
-        camera.setPipeline(pipeline);
+        camera.setPipeline(vision);
 
         camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
@@ -34,10 +37,32 @@ public class Barcode extends OpMode {
                 telemetry.addData("Status", "An error occurred with OpenCV!");
             }
         });
-    }
 
-    @Override
-    public void loop() {
 
+        if (robot == 1) {
+            telemetry.addData("Status", "Left");
+        } else if (robot == 2) {
+            telemetry.addData("Status", "Right");
+        } else if (robot == 3) {
+            telemetry.addData("Status", "Center");
+        } else {
+            telemetry.addData("Status", "None");
+        }
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.update();
+
+            if ((vision.getLeftValue() > vision.getRightValue()) && (vision.getLeftValue() > vision.getCenterValue())) {
+                telemetry.addData("Status", "Left");
+            } else if ((vision.getRightValue() > vision.getLeftValue()) && (vision.getRightValue() > vision.getCenterValue())) {
+                telemetry.addData("Status", "Right");
+            } else if ((vision.getCenterValue() > vision.getRightValue()) && (vision.getCenterValue() > vision.getLeftValue())) {
+                telemetry.addData("Status", "Center");
+            } else {
+                telemetry.addData("Status", "None");
+            }
+         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/computer_vision/opencv/team_element/YCbCrChannelTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/computer_vision/opencv/team_element/YCbCrChannelTest.java
@@ -8,6 +8,15 @@ import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 public class YCbCrChannelTest extends OpenCvPipeline {
+    public enum RobotPosition {
+        LEFT,
+        RIGHT,
+        CENTER,
+        NONE
+    }
+
+    public RobotPosition robotPosition;
+
     public int position;
     Mat YCrCb  = new Mat();
     Mat outPut = new Mat();
@@ -22,51 +31,75 @@ public class YCbCrChannelTest extends OpenCvPipeline {
 
     Scalar rectColor = new Scalar(255.0, 0.0, 0.0);
 
+    private final Object sync = new Object();
+
     @Override
     public Mat processFrame(Mat input) {
-        Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
+        synchronized (sync) {
+            Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
 
-        Rect leftRect = new Rect(1, 60, 80, 120);
-        Rect centerRect = new Rect(80, 60, 80, 120);
-        Rect rightRect = new Rect(160, 60, 80, 120);
+            Rect leftRect = new Rect(1, 60, 80, 120);
+            Rect centerRect = new Rect(80, 60, 80, 120);
+            Rect rightRect = new Rect(160, 60, 80, 120);
 
-        input.copyTo(outPut);
-        Imgproc.rectangle(outPut, leftRect, rectColor, 2);
-        Imgproc.rectangle(outPut, rightRect, rectColor, 2);
-        Imgproc.rectangle(outPut, centerRect, rectColor, 2);
+            input.copyTo(outPut);
+            Imgproc.rectangle(outPut, leftRect, rectColor, 2);
+            Imgproc.rectangle(outPut, rightRect, rectColor, 2);
+            Imgproc.rectangle(outPut, centerRect, rectColor, 2);
 
-        leftMat = YCrCb.submat(leftRect);
-        rightMat = YCrCb.submat(rightRect);
-        centerMat = YCrCb.submat(centerRect);
+            leftMat = YCrCb.submat(leftRect);
+            rightMat = YCrCb.submat(rightRect);
+            centerMat = YCrCb.submat(centerRect);
 
-        Core.extractChannel(leftMat, leftMat, 1);
-        Core.extractChannel(rightMat, rightMat, 1);
-        Core.extractChannel(centerMat, centerMat, 1);
+            Core.extractChannel(leftMat, leftMat, 1);
+            Core.extractChannel(rightMat, rightMat, 1);
+            Core.extractChannel(centerMat, centerMat, 1);
 
-        Scalar leftAvg = Core.mean(leftMat);
-        Scalar rightAvg = Core.mean(rightMat);
-        Scalar centerAvg = Core.mean(centerMat);
+            Scalar leftAvg = Core.mean(leftMat);
+            Scalar rightAvg = Core.mean(rightMat);
+            Scalar centerAvg = Core.mean(centerMat);
 
-        leftValue = Math.round(leftAvg.val[0]);
-        rightValue = Math.round(rightAvg.val[0]);
-        centerValue = Math.round(centerAvg.val[0]);
+            leftValue = Math.round(leftAvg.val[0]);
+            rightValue = Math.round(rightAvg.val[0]);
+            centerValue = Math.round(centerAvg.val[0]);
+        }
 
         if ((leftValue > rightValue) && (leftValue > centerValue)) {
-            //telemetry.addLine("The Object is on the Left");
-            position = 0;
-        } else if ((rightValue > leftValue) && (rightValue > centerValue)) {
-            //telemetry.addLine("The Object ios on the Right");
             position = 1;
-        } else if ((centerValue > leftValue) && (centerValue > rightValue)) {
-            //telemetry.addLine("The object is on the center");
+            robotPosition = RobotPosition.LEFT;
+        } else if ((rightValue > leftValue) && (rightValue > centerValue)) {
             position = 2;
-        } else {
-            //telemetry.addLine("There are no objects");
+            robotPosition = RobotPosition.RIGHT;
+        } else if ((centerValue > leftValue) && (centerValue > rightValue)) {
             position = 3;
+            robotPosition = RobotPosition.CENTER;
+        } else {
+            position = 4;
+            robotPosition = RobotPosition.NONE;
         }
 
         return (outPut);
     }
+
+    public RobotPosition getRobotPosition() {
+        return this.robotPosition;
+    }
+
+    public double getLeftValue() {
+        synchronized (sync) {
+            return leftValue;
+        }
+    }
+
+    public double getRightValue() {
+        synchronized (sync) {
+            return rightValue;
+        }
+    }
+
+    public double getCenterValue() {
+        synchronized (sync) {
+            return centerValue;
+        }
+    }
 }
-
-

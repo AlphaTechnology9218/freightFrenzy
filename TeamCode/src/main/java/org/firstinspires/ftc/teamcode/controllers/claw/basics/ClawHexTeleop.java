4b85f9370278a2499946b40e122b35121b818607
Eduardo <eduardomelodefarias@gmail.com>
2022-05-23 16:57:48 -0300

    Close shut the jaw of the Phoenix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/claw/basics/ClawHexTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/controllers/claw/basics/ClawHexTeleop.java
@@ -2,12 +2,14 @@ package org.firstinspires.ftc.teamcode.controllers.claw.basics;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.robot_components.HexClawComponents;
 import org.firstinspires.ftc.teamcode.robot_components.MotorComponentsClaw;
 
 @TeleOp(name = "Claw Hex Teleop", group = "Controllers")
 public class ClawHexTeleop extends OpMode {
-    MotorComponentsClaw motors = new MotorComponentsClaw();
+    HexClawComponents motors = new HexClawComponents();
 
     @Override
     public void init() {
@@ -16,8 +18,17 @@ public class ClawHexTeleop extends OpMode {
 
     @Override
     public void loop() {
-
+        jaw();
+        telemetry.addData("Position =", motors.sPin.getPosition());
+        telemetry.update();
     }
 
-
+    public void jaw(){
+        if(gamepad1.x){
+            motors.sPin.setPosition(0);
+        }
+        if(gamepad1.b){
+            motors.sPin.setPosition(60);
+        }
+    }
 }
